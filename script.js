0bbcef42f5c6f30d0182fc67d57c8c6908c6cbb9
Bernardo Macedo <bertnan92@gmail.com>
2026-03-10 17:02:33 -0300

    prototipo de site pra estudo

---
+++ script.js
@@ -0,0 +1,11 @@
+function clicou(){
+    alert("curioso demais jão")
+}
+
+function clicou2(){
+    alert("tenta o outro")
+}
+
+function redirecionar(){
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+}
